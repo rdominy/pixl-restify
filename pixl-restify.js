0d57b040e66797b2117f63d4f887d7b0497dfa66
Robert Dominy <robert.dominy@sincrodigital.com>
2022-04-04 18:10:28 -0700

    Modernize (let, const, arrow fns), linter cleanup

--- pixl-restify.js
+++ pixl-restify.js
@@ -6,7 +6,7 @@
 const Component = require("pixl-server/component");
 
 class PixlRestify extends Component {
-	constructor(logger) {
+	constructor() {
 		super();		
 		this.__name = "PixlRestify";
 		this.defaultConfig = {
@@ -17,7 +17,6 @@ class PixlRestify extends Component {
 				"delete" : "delete"
 			}
 		};
-
 	}
 	
 	/* 
@@ -27,9 +26,9 @@ class PixlRestify extends Component {
 		value: method handler to call
 	*/
 	createEmptyDispatchMap() {
- 		var dispatchMap = new Map();
-		var methodMap = this.config.get('methodMap');
- 		for (var method in methodMap) {
+ 		let dispatchMap = new Map();
+		const methodMap = this.config.get('methodMap');
+ 		for (const method in methodMap) {
  			dispatchMap.set(methodMap[method], new Map());
  		}
 		return dispatchMap;
@@ -41,7 +40,7 @@ class PixlRestify extends Component {
 	 Regular: {"code":0, yourResults...}
 	*/
 	static getResponseObject(err, resultObj) {
-		var response = null;
+		let response = null;
 
 		if (err) {
 			response = {code: "unknown", message: "no details"};
@@ -88,15 +87,14 @@ class PixlRestify extends Component {
 	}
 	
 	/*
-		Called during webserver startup, look for confingured endpoints and register the URL handlers
+		Called during webserver startup, look for configured endpoints and register the URL handlers
 	*/
 	startup(callback) {
 		this.logger = this.server.logger;
-		var self = this;
-		
+
 		if (this.server.WebServer && this.config.get('endpoints')) { 
-			this.config.get('endpoints').forEach(function(item) {
-				self.registerEndpoints(self.server[item.name], self.server.WebServer, new RegExp(item.regex));
+			this.config.get('endpoints').forEach((item) =>  {
+				this.registerEndpoints(this.server[item.name], this.server.WebServer, new RegExp(item.regex));
 			})
 		}
 
@@ -105,15 +103,12 @@ class PixlRestify extends Component {
 	
 	//	Inspect component properties looking for methods with correct method prefix and create a mapping of endpoint handlers
 	registerEndpoints(serviceObj, pixlWebServer, regex) {
-		var method= null;
-		var names = Object.getOwnPropertyNames(Object.getPrototypeOf(serviceObj));
-		var self = this;
+		const names = Object.getOwnPropertyNames(Object.getPrototypeOf(serviceObj));
+		let dispatchMap = this.createEmptyDispatchMap();
 
-		var dispatchMap = this.createEmptyDispatchMap();
-
-		names.forEach(function(method) {
+		names.forEach((method) => {
 			if (typeof serviceObj[method] == 'function') {
-				var mapping = self.mapMethod(method);
+				let mapping = this.mapMethod(method);
 				if (mapping!=null) {
 				dispatchMap.get(mapping.httpMethod).set(mapping.endPointName, {
 						name: method,
@@ -124,26 +119,26 @@ class PixlRestify extends Component {
 		});
 
 		if (this.logger.shouldLog(6)) {
-			var out = {};
-			for (var [key, value] of dispatchMap) {
+			let out = {};
+			for (const [key, value] of dispatchMap) {
 				out[key] = {};
-				for (var [method, endpoint] of value) {
+				for (const [method, endpoint] of value) {
 					out[key][method] = endpoint.name;
 				}
 			}
 			this.logger.debug(6, "DispatchMap for " + regex.toString(), out);
 		}
 
-		pixlWebServer.addURIHandler(regex, serviceObj.__name, false, function(args, httpCallback) {
-				self.dispatch(args, httpCallback, dispatchMap, regex);
-		});	
+		pixlWebServer.addURIHandler(regex, serviceObj.__name, false, (args, httpCallback) => {
+				this.dispatch(args, httpCallback, dispatchMap, regex);
+			});	
 	}
 	
 	// Test if method prefix is one of the configured HTTP methods and return info to be stored in the map
 	mapMethod(methodName) {
-		var results = null;
-		var methodMap = this.config.get('methodMap');
-		for (var methodPrefix in methodMap) {
+		let results = null;
+		const methodMap = this.config.get('methodMap');
+		for (const methodPrefix in methodMap) {
 			if (methodName.indexOf(methodPrefix)==0) {
 				results = {
 					httpMethod: methodMap[methodPrefix],
@@ -158,54 +153,54 @@ class PixlRestify extends Component {
 	// Called by pixl-server-web to handle HTTP requests
 	// This method finds the matching handler, calls it and packages up the JSON response
 	async dispatch(args, httpCallback, dispatchMap, regex) {
-		var method = args.request.method.toLowerCase();
+		const method = args.request.method.toLowerCase();
 		// First find mappings the the HTTP method (GET, POST, etc.)
 		if (dispatchMap.has(method)) {
-			var matches = args.request.url.match(regex);
+			const matches = args.request.url.match(regex);
 			
 			// Next see if we have an endpoint that handles the operation for the extracted object
 			if (matches && matches.length > 1 && dispatchMap.get(method).has(matches[1])) {
-				var endpoint = dispatchMap.get(method).get(matches[1]);
-				var destArgs = (method == 'get') ? args.query : args.params;
+				const endpoint = dispatchMap.get(method).get(matches[1]);
+				const destArgs = (method == 'get') ? args.query : args.params;
 				
-					this.logger.debug(8, "Dispatching HTTP call for " + args.request.url, destArgs);
-					var results = null;
-					var err = null;
+				this.logger.debug(8, "Dispatching HTTP call for " + args.request.url, destArgs);
+				let results = null;
+				let err = null;
+				
+				try {
+					// If the endpoint has a validator pass the input arguments into that
+					let isValid = true;
+					if (endpoint.validate) {
+						const validation = endpoint.validate(destArgs, args); // Method name is passed first by earlier binding
+						if (validation!==true) {
+							isValid = false;
+							err = validation;
+						}
+					}
 					
-					try {
-						// If the endpoint has a validator pass the input arguments into that
-						var isValid = true;
-						if (endpoint.validate) {
-							var validation = endpoint.validate(destArgs, args); // Method name is passed first by earlier binding
-							if (validation!==true) {
-								isValid = false;
-								err = validation;
-							}
-						}
-						
-						if (isValid) {
-							// Validation passed, attempt to call the handler
-							results = await endpoint.handler(destArgs, args);
-							this.logger.debug(8, "Results of HTTP call for " + args.request.url, results);
-						}
+					if (isValid) {
+						// Validation passed, attempt to call the handler
+						results = await endpoint.handler(destArgs, args);
+						this.logger.debug(8, "Results of HTTP call for " + args.request.url, results);
+					}
 
-						results = PixlRestify.getResponseObject(err, results);				
-					}
-					catch (e) {
-							results = PixlRestify.getResponseObject(e, null);
-							this.logger.error(e.code, "Error calling " + args.request.url, {err: results, stack: (e.stack)?JSON.stringify(e.stack):""});
-					}
-					
-					httpCallback(results);
+					results = PixlRestify.getResponseObject(err, results);				
+				}
+				catch (e) {
+					results = PixlRestify.getResponseObject(e, null);
+					this.logger.error(e.code, "Error calling " + args.request.url, {err: results, stack: (e.stack)?JSON.stringify(e.stack):""});
+				}
+				
+				httpCallback(results);
 			}
 			else {
-				var message = "No handler found for URI: " + args.request.url;
+				const message = "No handler found for URI: " + args.request.url;
 				this.logger.debug(5, `PixlRestify.dispatch error ${message}`);
 				httpCallback({code: 'no_handler', message: message} );
 			}
 		}
 		else {
-			var message = "unsupported method: " + method + " url: " + args.request.url;
+			const message = "unsupported method: " + method + " url: " + args.request.url;
 			this.logger.debug(5, `PixlRestify.dispatch error  ${message}`);
 			httpCallback({code: 'unsupported_method', message: message} );
 		}
